b5e0257c6dc5fab0ebe9382d0461311409695175
HasinJ <hasinchoudhuryj@gmail.com>
2020-12-22 16:39:18 -0500

    all testcases

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,11 @@
 const fibonacci = function(number) {
   number = parseInt(number, 10);
   if(number<0) return "OOPS";
-}
+  if(number<=2) return 1;
 
-//module.exports = fibonacci
+  let array = [1,1];
+  while(array.push(array[array.length-2]+array[array.length-1])!=number) continue;
+  return array[number-1];
+}
+// 0 1 1 2 3 5 8
+module.exports = fibonacci
